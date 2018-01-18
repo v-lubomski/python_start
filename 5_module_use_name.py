d85057b49dee2a4320fb4621349666bae50125e8
Vladislav <jiogvocal@gmail.com>
2018-01-18 19:54:49 +0400

    Added module lesson exemples

---
+++ 5_module_use_name.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+	print('This program launched itself')
+else:
+	print('I was imported in other module')
---
+++ 5_mymodule.py
@@ -0,0 +1,6 @@
+def sayhi():
+	print('Hello! Im a speaking module, marafaka! Stay here and I will not touch you, maaaan!')
+
+__version__ = '0.1'
+
+#end of module mymodule.py
